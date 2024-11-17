c4099cd9f65f113d07bac34752177f0f0de4a681
shamglam <shampo@hotmail.com>
2024-11-17 18:04:37 +0100

    oppdatert oppgavetekst

--- oppgave3.py
+++ oppgave3.py
@@ -1,2 +1,2 @@
 #denne gang skal du spørre om fire tall som er teller og nevner for to brøker og multiplisere brøkene"
-
+# legg inn sjekk slik at ingen av tallene er mindre enn 1 og større enn 100 
